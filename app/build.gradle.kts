131f4969c9c20cd8abf2dd1dbcdd21544281ce45
Dominik Grzelak <dominik.grzelak@allegro.pl>
2022-04-09 15:25:42 +0200

    Pass configuration through cli arguments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ val jacksonVersion = "2.13.1"
 dependencies {
     implementation(platform(kotlin("bom")))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
--- app/src/main/kotlin/pl/allegro/devskiller/App.kt
+++ app/src/main/kotlin/pl/allegro/devskiller/App.kt
@@ -1,22 +1,29 @@
 package pl.allegro.devskiller
 
-import pl.allegro.devskiller.config.assessments.SlackNotifierConfiguration
-import pl.allegro.devskiller.config.assessments.SlackNotifierProperties
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.required
 import pl.allegro.devskiller.config.assessments.CandidatesConfiguration
 import pl.allegro.devskiller.config.assessments.DevSkillerProperties
+import pl.allegro.devskiller.config.assessments.SlackNotifierConfiguration
+import pl.allegro.devskiller.config.assessments.SlackNotifierProperties
 import pl.allegro.devskiller.domain.assessments.NotifierService
 
-fun main() {
+fun main(args: Array<String>) {
+    val parser = ArgParser("<this_executable>")
+    val slackChannel by parser.option(ArgType.String).required()
+    val slackToken by parser.option(ArgType.String).required()
+    val devskillerToken by parser.option(ArgType.String).required()
+    parser.parse(args)
+
+    val devskillerProperties = DevSkillerProperties("https://api.devskiller.com/", devskillerToken)
     val configuration = CandidatesConfiguration()
     val candidateProvider = configuration.candidateProvider(
         httpClient = configuration.httpClient(),
-        devSkillerConfiguration = DevSkillerProperties("https://api.devskiller.com/", "CHANGE_ME")
+        devSkillerConfiguration = devskillerProperties
     )
 
-    val slackProperties = SlackNotifierProperties(
-        "C01DTCUUH55",
-        "xoxb-1469436098135-3165783184564-mbeMlLdEZbn8TMPvXymcd6DG"
-    )
+    val slackProperties = SlackNotifierProperties(slackChannel, slackToken)
     val slackConfig = SlackNotifierConfiguration(slackProperties)
     val notifier = slackConfig.slackAssessmentsNotifier()
     val notifierService = NotifierService(notifier, candidateProvider)
