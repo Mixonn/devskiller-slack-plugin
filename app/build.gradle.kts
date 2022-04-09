18d672c7ae88be26c2cf5aa7ae5b5d13ad91593e
Osipiuk Bartosz <osipiuk.bartosz@gmail.com>
2022-04-09 18:10:05 +0200

    Remove yaml dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
     implementation("com.slack.api:bolt:$boltVersion")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("org.slf4j:slf4j-simple:$slf4jVersion")
