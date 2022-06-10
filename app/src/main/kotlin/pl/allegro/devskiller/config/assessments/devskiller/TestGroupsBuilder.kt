d8fbc0211b54ba376fb61db007cde6dcc52c04a9
Osipiuk Bartosz <osipiuk.bartosz@gmail.com>
2022-06-10 15:53:34 +0200

    CR Fix

--- app/src/main/kotlin/pl/allegro/devskiller/config/assessments/devskiller/TestGroupsBuilder.kt
+++ app/src/main/kotlin/pl/allegro/devskiller/config/assessments/devskiller/TestGroupsBuilder.kt
@@ -9,15 +9,15 @@ import pl.allegro.devskiller.domain.assessments.provider.TestId
 class TestGroupsBuilder {
     companion object {
         fun fromString(groupsString: String): TestGroups = groupsString.split(";").associate { groupString ->
-            val splittedGroupString: Queue<String> = LinkedList(groupString.split(","))
-            val testName = splittedGroupString.poll()
-            val groupNotifyString = if (splittedGroupString.peek()?.startsWith("@") == true) {
-                splittedGroupString.poll().substring(1)
+            val splitGroupString: Queue<String> = LinkedList(groupString.split(","))
+            val testName = splitGroupString.poll()
+            val groupNotifyString = if (splitGroupString.peek()?.startsWith("@") == true) {
+                splitGroupString.poll().substring(1)
             } else {
                 null
             }
             val testDefinition = TestGroup(name = testName, notifyGroupName = groupNotifyString)
-            testDefinition to splittedGroupString.map { TestId(it) }
+            testDefinition to splitGroupString.map { TestId(it) }
         }.filter { it.key.name.isNotBlank() }.let { TestGroups(it) }
     }
 }
